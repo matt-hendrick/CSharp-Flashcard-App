6c14bfad764d5c256cc73642d3cf33df1cd87c30
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-09-05 10:50:16 -0400

    Add project files.

---
+++ FlashcardLibrary/Models/CardModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashcardLibrary.Models
+{
+    class CardModel
+    {
+    }
+}
---
+++ FlashcardUI/CreateAccountForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FlashcardUI
+{
+    public partial class CreateAccountForm : Form
+    {
+        public CreateAccountForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
